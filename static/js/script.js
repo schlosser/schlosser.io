6883bcd03938b1af9a6d85a6e7fc7e9e54a4578e
Dan <dan.r.schlosser@gmail.com>
2013-05-03 13:50:48 -0400

    update blog

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,17 @@
-$(document).ready(function(){
+$(function(){
     //blog rendering
-    var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
-    function loadFile() {
-        console.log("loadFile")
-        reader.open('get', "static/blog/2013-04-05.html", true); 
-        reader.onreadystatechange = displayContents;
-        reader.send(null);
-    }
-    function displayContents() {
-        if(reader.readyState==4) {
-            var post = $('<div>').addClass('post')
-                                 .html(reader.responseText);
-            //console.log(post.html);
-            $('#blog-posts').append(post);
-        }
-    }
-    loadFile();
+    var blog_dir = "static/blog/";
+    $.get(blog_dir+"file_list.txt", function(data){
+        $.each(data.split("\n"), function(i, val){
+            $.get(blog_dir+val, function(data){
+                var post = $('<div>', {"class":"post", html:data});
+                //console.log(post.html);
+                $('#blog-posts').append($('<hr/>'));
+                $('#blog-posts').append(post);               
+            });
+        });
+    });
+           
     
     //Slashes to Hashes
     var pages = ['home', 'about', 'projects', 'resources', 'blog'];
