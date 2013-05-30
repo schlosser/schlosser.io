644c50570ebf3bac689cc27ca3bfa2e15aae087b
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-05-30 14:12:36 -0400

    Revert "testing blueprinting"
    This reverts commit 594edcbcd3e865732e72ae2a5f0a2d2df5980237.

--- app.py
+++ app.py
@@ -1,61 +1,50 @@
-from flask import Flask, Blueprint, Response, make_response, redirect, render_template, \
+from flask import Flask, Response, make_response, redirect, render_template, \
 	request, url_for, send_from_directory
 
 app = Flask(__name__)
-app.config['SERVER_NAME'] = 'danrs.ch'
-app.url_map.default_subdomain = 'www'
 
-www = Blueprint('www', 'www', subdomain='www')
-# apps = Blueprint('apps', 'apps', subdomain='apps')
-# 
-# @apps.route('/')
-# def apps():
-# 	return render_template('apps.html')
-
-@www.route('/rss.xml')
-def rss():
-	return send_from_directory(app.static_folder, request.path[1:])
+class routes:
 	
-@www.route('/admin/')
-def admin():
-	return render_template('admin.html')
-
-@www.route('/blog/<post_id>/')
-def post(post_id=""):
-	d = dict(page='blog',post_id=post_id)
-	return redirect(url_for('home', **d))
-
-@www.route('/r/<resource>/')
-def resource(resource=""):
-	return render_template('iframe.html', resource=resource)
-
-	@www.route('/dfa-demo/')
-	def dfa():
-		return render_template('dfa-demo.html')
+	@app.route('/rss.xml')
+	def rss():
+		return send_from_directory(app.static_folder, request.path[1:])
+		
+	@app.route('/admin/')
+	def admin():
+		return render_template('admin.html')
 	
-@www.route('/<tab>/')
-def yay(tab=""):
-	redirects = ['dfa-demo', 'admin']
-	if tab in redirects:
-		return redirect(url_for(tab))
-	d = dict(page=tab)
-	return redirect(url_for('home', **d))
-
-@www.route('/favicon.ico')
-def icon():
-	return url_for('static', filename='img/favicon.ico')
+	@app.route('/blog/<post_id>/')
+	def post(post_id=""):
+		d = dict(page='blog',post_id=post_id)
+		return redirect(url_for('home', **d))
 	
-@www.route('/')
-def home():
-	keys = request.args.keys()
-	if len(keys):
-		request.args.get("post_id")
-		return render_template('index.html', page=request.args.get("page"), post_id=request.args.get("post_id"))
-	return render_template('index.html')
+	@app.route('/r/<resource>/')
+	def resource(resource=""):
+		return render_template('iframe.html', resource=resource)
+	
+ 	@app.route('/dfa-demo/')
+ 	def dfa():
+ 		return render_template('dfa-demo.html')
+		
+	@app.route('/<tab>/')
+	def yay(tab=""):
+		redirects = ['dfa-demo', 'admin']
+		if tab in redirects:
+			return redirect(url_for(tab))
+		d = dict(page=tab)
+		return redirect(url_for('home', **d))
 
-
-app.register_blueprint(www)
-# app.register_blueprint(apps)
+	@app.route('/favicon.ico')
+	def icon():
+		return url_for('static', filename='img/favicon.ico')
+		
+	@app.route('/')
+	def home():
+		keys = request.args.keys()
+		if len(keys):
+			request.args.get("post_id")
+			return render_template('index.html', page=request.args.get("page"), post_id=request.args.get("post_id"))
+		return render_template('index.html')
 	
 if __name__ == '__main__':
     app.run(debug=True)
