ab30283f4b28a3764f5f174c35b18c474813b383
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-06-28 15:18:41 -0400

    tryin to pull

--- static/js/script.js
+++ static/js/script.js
@@ -56,23 +56,6 @@ $(function(){
     
     
     
-    //Konami Code
-    $('.konami-box').hide();
-    //Breaks mobile
-    /* function rotate() {
-        var $konami = $("#konami");
-        rotate(0);
-        function rotate(degree) {        
-            $konami.css({ WebkitTransform: 'rotate(' + degree + 'deg)'});  
-            $konami.css({ '-moz-transform': 'rotate(' + degree + 'deg)'});                      
-            timer = setTimeout(function() {
-                rotate(++degree);
-            },5);
-        };
-    };
-    var konami = new Konami(function(){
-        $('.konami-box').show(rotate) //<--breaks mobile
-    });   */
     
     //Slashes to Hashes
     
@@ -185,40 +168,93 @@ $(function(){
     };
     
     //Twitter Configuration
-    $.getJSON(
-        'http://api.twitter.com/1.1/statuses/user_timeline.json?callback=?&screen_name=danrschlosser&count=4',
-        function (data)
-        {
-            var $tweets = $('#tweets');
-            $tweets.empty();
-            $tweets.append($('<hr/>'));
-            if (data.length !== 0) {
-                $.each(data, function (i, tweet)
-                {
-                    if (tweet.text !== undefined) {
-                        var text = tweet.text.replace(/[A-Za-z]+:\/\/[A-Za-z0-9-_]+\.[A-Za-z0-9-_:%&\?\/.=]+/g, function(url) { 
-                            var wrap = document.createElement('div');
-                            var anch = document.createElement('a');
-                            anch.href = url;
-                            anch.target = "_blank";
-                            anch.innerHTML = url;
-                            wrap.appendChild(anch);
-                            return wrap.innerHTML;
-                        });
-                        text = text.replace(/(^|\s)@(\w+)/g, '$1<a href="http://twitter.com/$2" target="_blank">@$2</a>');
-                        text = text.replace(/(^|\s)#(\w+)/g, '$1<a href="http://search.twitter.com/search?q=%23$2" target="_blank">#$2</a>');
-                        $tweets.append($('<div class="social-update">' + text + '</div>'));
-                        $tweets.append($("<div>").addClass("post-time")
-                            .html($('<a>')
-                            .text($.timeago(tweet['created_at']))
-                            .attr('href', 'https://twitter.com/danrschlosser/status/' + data['id_str'])));
-                        $tweets.append($('<hr/>'));
-                    }
-                });
-            } else {
-                $tweets.append($('<div class="social-update">No recent tweets</div>'));
-            }
-            $tweets.append($('<a class="social-update" href="http://www.twitter.com/danrschlosser">View more tweets...</a>'));
-        }
-    );
+    /*********************************************************************
+	*  #### Twitter Post Fetcher v7.0 ####
+	*  Coded by Jason Mayes 2013. A present to all the developers out there.
+	*  www.jasonmayes.com
+	*  Please keep this disclaimer with my code if you use it. Thanks. :-)
+	*  Got feedback or questions, ask here: 
+	*  http://www.jasonmayes.com/projects/twitterApi/
+	*  Updates will be posted to this site.
+	*********************************************************************/
+	var twitterFetcher = function () {
+	    function t(d) {
+	        return d.replace(/<b[^>]*>(.*?)<\/b>/gi, function (c, d) {
+	            return d
+	        }).replace(/class=".*?"|data-query-source=".*?"|dir=".*?"|rel=".*?"/gi, "")
+	    }
+	
+	    function m(d, c) {
+	        for (var f = [], e = RegExp("(^| )" + c + "( |$)"), g = d.getElementsByTagName("*"), b = 0, a = g.length; b < a; b++) e.test(g[b].className) && f.push(g[b]);
+	        return f
+	    }
+	    var u = "",
+	        j = 20,
+	        n = !0,
+	        h = [],
+	        p = !1,
+	        k = !0,
+	        l = !0,
+	        q = null,
+	        r = !0;
+	    return {
+	        fetch: function (d, c, f, e, g, b, a) {
+	            void 0 === f && (f = 20);
+	            void 0 === e && (n = !0);
+	            void 0 === g && (g = !0);
+	            void 0 === b && (b = !0);
+	            void 0 === a && (a = "default");
+	            p ? h.push({
+	                id: d,
+	                domId: c,
+	                maxTweets: f,
+	                enableLinks: e,
+	                showUser: g,
+	                showTime: b,
+	                dateFunction: a
+	            }) : (p = !0, u = c, j = f, n = e, l = g, k = b, q = a, c = document.createElement("script"), 
+	            c.type = "text/javascript", 
+	            c.src = "//cdn.syndication.twimg.com/widgets/timelines/" + d + "?&lang=en&callback=twitterFetcher.callback&suppress_response_codes=true&rnd=" + Math.random(), 
+	            document.getElementsByTagName("head")[0].appendChild(c))
+	        },
+	        callback: function (d) {
+	            var c = document.createElement("div");
+	            c.innerHTML = d.body;
+	            "undefined" ===
+	                typeof c.getElementsByClassName && (r = !1);
+	            var f = d = null,
+	                e = null;
+	            r ? (d = c.getElementsByClassName("e-entry-title"), f = c.getElementsByClassName("p-author"), e = c.getElementsByClassName("dt-updated")) : (d = m(c, "e-entry-title"), f = m(c, "p-author"), e = m(c, "dt-updated"));
+	            for (var c = [], g = d.length, b = 0; b < g;) {
+	                if ("string" !== typeof q) {
+	                    var a = new Date(e[b].getAttribute("datetime").replace(/-/g, "/").replace("T", " ").split("+")[0]),
+	                        a = q(a);
+	                    e[b].setAttribute("aria-label", a);
+	                    if (d[b].innerText)
+	                        if (r) e[b].innerText = a;
+	                        else {
+	                            var s = document.createElement("p"),
+	                                v = document.createTextNode(a);
+	                            s.appendChild(v);
+	                            s.setAttribute("aria-label", a);
+	                            e[b] = s
+	                        } else e[b].textContent = a
+	                }
+	                n ? (a = "", l && (a += '<div class="user">' + t(f[b].innerHTML) + "</div>"), a += '<p class="tweet">' + t(d[b].innerHTML) + "</p>", k && (a += '<p class="timePosted">' + e[b].getAttribute("aria-label") + "</p>")) : d[b].innerText ? (a = "", l && (a += '<p class="user">' + f[b].innerText + "</p>"), a += '<p class="tweet">' + d[b].innerText + "</p>", k && (a += '<p class="timePosted">' + e[b].innerText + "</p>")) : (a = "", l && (a += '<p class="user">' + f[b].textContent +
+	                    "</p>"), a += '<p class="tweet">' + d[b].textContent + "</p>", k && (a += '<p class="timePosted">' + e[b].textContent + "</p>"));
+	                c.push(a);
+	                b++
+	            }
+	            c.length > j && c.splice(j, c.length - j);
+	            d = c.length;
+	            f = 0;
+	            e = document.getElementById(u);
+	            for (g = "<ul>"; f < d;) g += "<li>" + c[f] + "</li>", f++;
+	            e.innerHTML = g + "</ul>";
+	            p = !1;
+	            0 < h.length && (twitterFetcher.fetch(h[0].id, h[0].domId, h[0].maxTweets, h[0].enableLinks, h[0].showUser, h[0].showTime, h[0].dateFunction), h.splice(0, 1))
+	        }
+	    }
+	}();
+    twitterFetcher.fetch('347442772464246785', 'tweets', 3, true);
 });
