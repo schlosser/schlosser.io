b247f7c54a1a3a8e91f83b26094817a902c6f478
Dan Schlosser <dan.r.schlosser@gmail.com>
2013-11-09 22:54:41 -0500

    update remotes script

--- scripts/add-remotes.sh
+++ scripts/add-remotes.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 ALREADY_EXISTS="already exists"
+SUCCESSFUL=$true
 
 printf "Adding origin... "
 ORIGIN_OUTPUT=$(git remote add origin dan@danrs.ch:~/danrs-ch.git 2>&1)
@@ -10,6 +11,7 @@ if [ ! -z "$ORIGIN_OUTPUT" ]; then
 
 	else
 		echo "Adding remote \"origin\" failed: " "$ORIGIN_OUTPUT"
+		SUCCESSFUL=$false
 	fi
 else
 	echo "Remote \"origin\" added successfully. (SUCCESS)"
@@ -22,9 +24,21 @@ if [ ! -z "$GITHUB_OUTPUT" ]; then
 		echo "Remote \"github\" already exists. (SUCCESS)"
 	else
 		echo "Adding remote \"origin\" failed: " "$ORIGIN_OUTPUT"
+		SUCCESSFUL=$false
 	fi
 else
 	echo " Remote \"github\" added successfully. (SUCCESS)"
 fi
 
-git remote -vvv
+if [ !$SUCCESSFUL ]; then
+	echo "$0 ran with errors. (FAILURE)"
+else
+	git remote -vvv
+	git fetch origin
+	git checkout -b origin/post-images
+	git branch --set-upstream-to=origin/post-images post-images
+	git checkout -b github
+	git branch --set-upstream-to=github/master github
+	git branch --set-upstream-to=origin/master master
+	git checkout master
+fi
