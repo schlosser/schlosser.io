fd3dd53c78de9023cebf9c259cd2294fef637e43
Dan Schlosser <dan.r.schlosser@gmail.com>
2014-02-27 21:39:10 -0500

    admin fixes

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,6 +1,9 @@
 $(function() {
 	if (Modernizr.touch) {
 		$(".delete").addClass("touch");
+		$(".admin-sentence .sentence-text").click(function() {
+			$(this).siblings("form").slideToggle(200);
+		});
 	}
 	$(".delete").click(function() {
 		$(this).siblings("form").slideToggle(200);
