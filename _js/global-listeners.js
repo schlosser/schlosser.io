6cbbba63a78e69771aefe38b3327f8c8087faa38
Dan Schlosser <dan.r.schlosser@gmail.com>
2022-05-30 17:14:09 -0400

    escaping new york post

--- _js/global-listeners.js
+++ _js/global-listeners.js
@@ -1,43 +1,42 @@
 function _addClass(e, c) {
- e.className = e.className.replace(c, '').replace('  ', ' ').trim() + ' ' + c;
+  e.className = e.className.replace(c, "").replace("  ", " ").trim() + " " + c;
 }
 
 function _removeClass(e, c) {
- e.className = e.className.replace(c, '').replace('  ', ' ').trim();
+  e.className = e.className.replace(c, "").replace("  ", " ").trim();
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  var body = document.getElementsByTagName('body')[0];
-  var html = document.getElementsByTagName('html')[0];
+document.addEventListener("DOMContentLoaded", function () {
+  var body = document.getElementsByTagName("body")[0];
+  var html = document.getElementsByTagName("html")[0];
   var wordState = 0;
 
   function enableDarkMode() {
-    _addClass(body, 'dark-mode');
-    html.style.backgroundColor = '#000000';
+    _addClass(body, "dark-mode");
+    html.style.backgroundColor = "#222222";
     localStorage.setItem("mode", "dark");
   }
 
   function disableDarkMode() {
-    _removeClass(body, 'dark-mode');
-    html.style.backgroundColor = '';
+    _removeClass(body, "dark-mode");
+    html.style.backgroundColor = "";
     localStorage.removeItem("mode");
   }
 
   function enableImageLinks() {
-    _addClass(body, 'show-image-links');
+    _addClass(body, "show-image-links");
   }
 
   function disableImageLinks() {
-    _removeClass(body, 'show-image-links');
+    _removeClass(body, "show-image-links");
   }
 
   if (localStorage.getItem("mode") === "dark") {
     enableDarkMode();
   }
 
-  document.addEventListener('keyup', function(e) {
-    switch(e.which) {
+  document.addEventListener("keyup", function (e) {
+    switch (e.which) {
       case 16: // "shift"
       case 18: // "alt"
       case 91: // "cmd"
@@ -47,10 +46,10 @@ document.addEventListener('DOMContentLoaded', function() {
         wordState = 1;
         break;
       case 65: // "a"
-        wordState = (wordState === 1) ? 2 : 0;
+        wordState = wordState === 1 ? 2 : 0;
         break;
       case 82: // "r"
-        wordState = (wordState === 2) ? 3 : 0;
+        wordState = wordState === 2 ? 3 : 0;
         break;
       case 75: // "k"
         if (wordState === 3) {
@@ -62,13 +61,13 @@ document.addEventListener('DOMContentLoaded', function() {
         wordState = 10;
         break;
       case 73: // "i"
-        wordState = (wordState === 10) ? 20 : 0;
+        wordState = wordState === 10 ? 20 : 0;
         break;
       case 71: // "g"
-        wordState = (wordState === 20) ? 30 : 0;
+        wordState = wordState === 20 ? 30 : 0;
         break;
       case 72: // "h"
-        wordState = (wordState === 30) ? 40 : 0;
+        wordState = wordState === 30 ? 40 : 0;
         break;
       case 84: // "t"
         if (wordState === 40) {
@@ -82,7 +81,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  document.addEventListener('keydown', function(e) {
+  document.addEventListener("keydown", function (e) {
     switch (e.which) {
       case 16: // "shift"
       case 18: // "alt"
