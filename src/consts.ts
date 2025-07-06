f88f51d12c5e060dc98f156470b425e710512824
Dan Schlosser <dan.r.schlosser@gmail.com>
2025-07-06 13:56:37 -0400

    Favicon and preview content

--- src/consts.ts
+++ src/consts.ts
@@ -1,28 +1,33 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dan Schlosser";
+export const SITE_DESCRIPTION =
+  "Co-founder, Ambrook — we're hiring! Previously, product manager at The New York Times, Google, and Covid Act Now, tech lead at The Next 50, founder and web developer at Minimill, and CS at Columbia. Email me at dan@schlosser.io or tweet @danrschlosser.";
+export const TWITTER_HANDLE = "@danrschlosser";
+export const DOMAIN = "schlosser.io";
+export const ROOT = `https://${DOMAIN}`;
+export const THEME_COLOR = "#222222";
 
 export const SOCIAL_LINKS = [
   {
-    "title": "Twitter",
-    "url": "https://www.twitter.com/danrschlosser"
+    title: "Twitter",
+    url: "https://www.twitter.com/danrschlosser",
   },
   {
-    "title": "GitHub",
-    "url": "https://github.com/schlosser"
+    title: "GitHub",
+    url: "https://github.com/schlosser",
   },
   {
-    "title": "Medium",
-    "url": "https://medium.com/@danrschlosser"
+    title: "Medium",
+    url: "https://medium.com/@danrschlosser",
   },
   {
-    "title": "LinkedIn",
-    "url": "https://www.linkedin.com/in/danrschlosser/"
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/danrschlosser/",
   },
   {
-    "title": "Email",
-    "url": "mailto:dan@schlosser.io"
-  }
+    title: "Email",
+    url: "mailto:dan@schlosser.io",
+  },
 ];
